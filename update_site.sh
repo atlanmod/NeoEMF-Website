a2e29d7bee0a37c085ef4d515d3138d969261b32
gwendal <gwendal.daniel@gmail.com>
2017-05-23 17:42:55 +0200

    update publish script

--- update_site.sh
+++ update_site.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 echo "Updating NeoEMF Website"
-cp _site -r _site /tmp/_site
+cp -r _site /tmp/_site
 git checkout gh-pages
+
+echo "Deleting Previous Build"
+rm -rf css img js resources vendor CNAME LICENCE index.html
+
+echo "Updating Build"
+mv /tmp/_site/* .
+rm -rf /tmp/_site
+
+echo "Committing changes"
+git add -A
+git commit -m "update website from master"
+
+git checkout master
