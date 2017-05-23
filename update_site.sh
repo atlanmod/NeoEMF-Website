7bc23b709e31610a53e8091ac8b5dc315dadf118
gwendal <gwendal.daniel@gmail.com>
2017-05-23 18:13:14 +0200

    update pusblish script

--- update_site.sh
+++ update_site.sh
@@ -3,16 +3,23 @@
 echo "Updating NeoEMF Website"
 cp -r _site /tmp/_site
 git checkout gh-pages
+res=$?
+if [ res != 0 ]
+then
+	echo "You have uncommitted changes on master, please commit your changes
+	before updating the website"
+	exit 1
+fi
 
 echo "Deleting Previous Build"
-#rm -rf css img js static vendor CNAME LICENCE index.html
+rm -rf css img js static vendor CNAME LICENCE index.html
 
-#echo "Updating Build"
-#mv /tmp/_site/* .
-#rm -rf /tmp/_site
+echo "Updating Build"
+mv /tmp/_site/* .
+rm -rf /tmp/_site
 
-#echo "Committing changes"
-#git add -A
-#git commit -m "update website from master"
+echo "Committing changes"
+git add -A
+git commit -m "update website from master"
 
-#git checkout master
+git checkout master
